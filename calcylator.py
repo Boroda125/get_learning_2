47c3bfd58787ca363ae18fb7498bcb57270df73f
Boroda125 <sergei.borodyn@gmail.com>
2024-12-02 20:00:06 +0300

    task2 - реализация деления

--- calcylator.py
+++ calcylator.py
@@ -1,9 +1,21 @@
+import traceback
+
+
 class Calcylator:
     last_rez = None
 
     def sum(self, n1, n2):
         self.last_rez = n1 + n2
         return self.last_rez
+
+    def divizion(self, n1, n2):
+        try:
+            rez = n1 / n2
+            self.last_rez = rez
+            return rez
+        except:
+          traceback.print_exc()
+
 
     def print_last_rez (self):
         print(self.last_rez)
