62e4bccc459d1847998a67ff8e835cc7a179b3f0
Boroda125 <sergei.borodyn@gmail.com>
2024-12-02 19:27:51 +0300

    Task1 - реализация умножения

--- test_calcylator.py
+++ test_calcylator.py
@@ -1,7 +1,9 @@
 from calcylator import Calcylator
+
 # Задача
 # Разработчику 1 - реализовать функцию умножения
 # Разработчику 2 - реализовать функцию деления
+
 calc = Calcylator()
 calc.sum(5, 10)
 calc.print_last_rez()
